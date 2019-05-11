3cb579ef6be491c7d3dbc42a75a20a375c11af92
Julio <jc.gutierrezq@gmail.com>
2019-05-10 21:11:05 -0500

    Testing with MockClient

--- lib/src/resources/news_api_provider.dart
+++ lib/src/resources/news_api_provider.dart
@@ -19,5 +19,4 @@ class NewsApiProvider {
 
     return ItemModel.fromJson(parsedJson);
   }
-  
 }
--- test/src/resources/news_api_provider_test.dart
+++ test/src/resources/news_api_provider_test.dart
@@ -5,12 +5,30 @@ import 'package:http/http.dart';
 import 'package:http/testing.dart';
 
 void main() {
-  test('FetchTopIds return a list of ids', () {
-    // setup of test case
-    final sum = 1 + 3;
+  test('FetchTopIds return a list of ids', () async {
+    final newsApi = NewsApiProvider();
 
-    // expectation
-    expect(sum, 1);
+    // MockClient, simula la conexión de htttp, para realizar pruebas.
+    newsApi.client = MockClient((request) async {
+      return Response(json.encode([1, 2, 3, 4]), 200);
+    });
 
+    final ids = await newsApi.fetchTopIds();
+
+    expect(ids, [1, 2, 3, 4]);
+  });
+
+  test('FetchItem returns a item model', () async {
+    final newsApi = NewsApiProvider();
+
+    // MockClient, define que cualquier llamada hará un return de {'id': 123}
+    newsApi.client = MockClient((request) async {
+      final jsonMap = {'id': 123};
+      return Response(json.encode(jsonMap), 200);
+    });
+
+    // Hace una llamada a la función enviando un valor, pero esperando un resultado definido en MockClient.
+    final item = await newsApi.fetchItem(999);
+    expect(item.id, 123);
   });
 }
