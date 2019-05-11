d80c6adec0f576e2ae95dc9900deb545e9d118db
Julio <jc.gutierrezq@gmail.com>
2019-05-10 20:24:26 -0500

    test

---
+++ test/src/resources/news_api_provider_test.dart
@@ -0,0 +1,16 @@
+import 'package:news_flutter/src/resources/news_api_provider.dart';
+import 'dart:convert';
+import 'package:test/test.dart';
+import 'package:http/http.dart';
+import 'package:http/testing.dart';
+
+void main() {
+  test('FetchTopIds return a list of ids', () {
+    // setup of test case
+    final sum = 1 + 3;
+
+    // expectation
+    expect(sum, 1);
+
+  });
+}
